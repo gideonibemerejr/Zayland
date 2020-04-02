4afcb2f81abdad33247c7a4e21c4d3aa2eb22a3a
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2020-04-02 02:56:56 -0500

    Update styles

--- nobody-knows/src/App/index.js
+++ nobody-knows/src/App/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { LandingPage, HomePage } from '../Pages';
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={LandingPage} />
-      <Route path='/home' component={HomePage} />
+      {/* <Route path='/home' component={HomePage} /> */}
     </Router>
   );
 }
--- nobody-knows/src/Pages/HomePage/index.jsx
+++ nobody-knows/src/Pages/HomePage/index.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 class HomePage extends Component {
   state = {};
   render() {
     return (
-      <div className='homepage--container'>
-          
-
-
-        <Switch>
-          <Route path='/listen' />
-          <Route path='/watch' />
-        </Switch>
-      </div>
+      //   <div className='homepage--container'>
+      <Switch>
+        <Route path='/listen' />
+        <Route path='/watch' />
+      </Switch>
+      //   </div>
     );
   }
 }
