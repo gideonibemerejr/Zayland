e704a2d4c64b1326d95061b6c2d5df9d9c6b6fe1
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2020-07-23 23:41:05 -0500

    Update nav link color

--- nobody-knows/src/Components/Nav/index.jsx
+++ nobody-knows/src/Components/Nav/index.jsx
@@ -5,7 +5,7 @@ const Nav = ({ toggleMenu }) => {
   return (
     <nav className='db dt-l w-100 border-box pa3 ph5-l relative'>
       <Link
-        className='db dtc-l v-mid white-80 link dim w-100 w-25-l tc tl-l mb2 mb0-l'
+        className='db dtc-l v-mid yellow link dim w-100 w-25-l tc tl-l mb2 mb0-l'
         to='/'
         title='Home'
       >
@@ -13,35 +13,35 @@ const Nav = ({ toggleMenu }) => {
       </Link>
       <div className='db dtc-l v-mid w-100 w-75-l tc tr-l'>
         <Link
-          className='link dim white-80 f6 f5-l fw5 dib mr3 mr4-l'
+          className='link dim yellow f6 f5-l fw5 dib mr3 mr4-l'
           to='/'
           title='Home'
         >
           HOME
         </Link>
         {/* <Link
-          className='link dim white-80 f6 f5-l fw5 dib mr3 mr4-l'
+          className='link dim yellow f6 f5-l fw5 dib mr3 mr4-l'
           to='/nobody-knows'
           title='Nobody Knows'
         >
           NOBODY KNOWS
         </Link>
         <Link
-          className='link dim white-80 f6 f5-l fw5 dib mr3 mr4-l'
+          className='link dim yellow f6 f5-l fw5 dib mr3 mr4-l'
           to='/credits'
           title='Credits'
         >
           CREDITS
         </Link> */}
         <Link
-          className='link dim white-80 f6 f5-l fw5 dib mr3 mr4-l'
+          className='link dim yellow f6 f5-l fw5 dib mr3 mr4-l'
           to='/watch'
           title='Watch'
         >
           WATCH
         </Link>
         {/* <a
-          className='pointer link dim white-80 f6 f5-l fw5 dib '
+          className='pointer link dim yellow f6 f5-l fw5 dib '
           href='https://shop.zaylandxx.com'
           title='Shop'
         >
@@ -50,9 +50,9 @@ const Nav = ({ toggleMenu }) => {
       </div>
     </nav>
     // <nav className='w-100 z-2'>
-    //   <ul className='list pa0 mt1 mb0 mh4 flex flex-row justify-between items-center-l white-80 f3  lh-copy fw7'>
+    //   <ul className='list pa0 mt1 mb0 mh4 flex flex-row justify-between items-center-l yellow f3  lh-copy fw7'>
     //     <li>
-    //       <Link to='/home' className='link white-80'>
+    //       <Link to='/home' className='link yellow'>
     //         <h1 className='ma0 f2 f3-l'>ZAYLAND</h1>
     //       </Link>
     //     </li>
