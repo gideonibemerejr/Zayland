1019a9b77cb9aa6a8fbf1f8f4d4ce17858aba78c
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2021-03-09 12:55:23 -0600

    updates

--- nobody-knows/src/Components/Home/index.jsx
+++ nobody-knows/src/Components/Home/index.jsx
@@ -1,37 +1,52 @@
-import React, { useEffect } from 'react';
-import { getGetAwayPlatforms } from '../../data';
-
-const platforms = getGetAwayPlatforms();
+import React, { useEffect } from "react";
+// import { getGetAwayPlatforms } from '../../data';
+import { EventbriteButton } from "../";
+import flyer from "../../assets/images/site-flyer.png";
+// const platforms = getGetAwayPlatforms();
 const Home = ({ setCurrentPage }) => {
-  useEffect(() => {
-    setCurrentPage('HOME');
-  }, [setCurrentPage]);
-  return (
-    <main className='w-100 vh-75 h-100-l flex justify-center items-center'>
-      <section className='w-100 flex flex-column  justify-center items-center mh5-l pa2 pa0-ns mt5-l'>
-        <div className='w-100 w-75-l h-75 mb2 flex justify-center items-center'>
-          <img
-            src='https://imgur.com/LRfH4Hv.jpg'
-            alt=''
-            className='w-50-ns w-100'
-          />
-        </div>
-        <div className='flex justify-center items-center mt4 pa2'>
-          {platforms.map((platform, idx) => (
-            <div key={platform.title} className='h-25 mh3'>
-              <a href={platform.link} rel='noopener noreferrer' target='_blank'>
-                <img
-                  className='h3-ns w3-ns h2 w2 dib'
-                  src={platform.iconSrc}
-                  alt=''
-                />
-              </a>
-            </div>
-          ))}
-        </div>
-      </section>
-    </main>
-  );
+	useEffect(() => {
+		setCurrentPage("HOME");
+	}, [setCurrentPage]);
+	return (
+		<main className="w-100 vh-100 h-100-l flex justify-center items-center">
+			<section className="w-100 flex flex-column  justify-center items-center mh5-l pa2 pa0-ns mt5-l">
+				<div className="w-100 w-75-l h-75 mb2 flex justify-center items-center">
+					<img src={flyer} alt="" className="w-50-ns w-100" />
+				</div>
+				<div className="flex flex-column justify-center items-center mt4 pa2">
+					<EventbriteButton
+						classNames="mr4"
+						// isLoading={}
+						ebEventId={145310251875}
+					>
+						Get Tickets
+					</EventbriteButton>
+					<p className="serif white f3-ns f4">
+						Powered by{" "}
+						<a
+							className="link hover-red white fw7 i"
+							href="https://whenwherewhat.com"
+							rel="nopenner noreferrer"
+							target="_blank"
+						>
+							When Where What
+						</a>
+					</p>
+					{/* {platforms.map((platform, idx) => (
+						<div key={platform.title} className="h-25 mh3">
+							<a href={platform.link} rel="noopener noreferrer" target="_blank">
+								<img
+									className="h3-ns w3-ns h2 w2 dib"
+									src={platform.iconSrc}
+									alt=""
+								/>
+							</a>
+						</div>
+					))} */}
+				</div>
+			</section>
+		</main>
+	);
 };
 
 export default Home;
--- nobody-knows/src/Pages/HomePage/index.jsx
+++ nobody-knows/src/Pages/HomePage/index.jsx
@@ -1,57 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Nav, Menu, Home, Watch } from '../../Components';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Nav, Menu, Home, Watch } from "../../Components";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class HomePage extends Component {
-  state = {
-    isMenuOpen: false,
-    currentPage: 'HOME',
-  };
+	state = {
+		isMenuOpen: false,
+		currentPage: "HOME",
+	};
 
-  toggleMenu = () => {
-    this.setState({ isMenuOpen: !this.state.isMenuOpen }, () => {
-      this.rotateIcon();
-    });
-  };
-  setCurrentPage = (currentPage) => {
-    this.setState({ currentPage });
-  };
-  rotateIcon = () => {
-    const plus = document.querySelector('#menu-icon');
+	toggleMenu = () => {
+		this.setState({ isMenuOpen: !this.state.isMenuOpen }, () => {
+			this.rotateIcon();
+		});
+	};
+	setCurrentPage = (currentPage) => {
+		this.setState({ currentPage });
+	};
+	rotateIcon = () => {
+		const plus = document.querySelector("#menu-icon");
 
-    if (this.state.isMenuOpen) {
-      plus.classList.add('rotate');
-    } else {
-      plus.classList.remove('rotate');
-    }
-  };
+		if (this.state.isMenuOpen) {
+			plus.classList.add("rotate");
+		} else {
+			plus.classList.remove("rotate");
+		}
+	};
 
-  renderMenu = () => {
-    return <Menu toggleMenu={this.toggleMenu} />;
-  };
+	renderMenu = () => {
+		return <Menu toggleMenu={this.toggleMenu} />;
+	};
 
-  render() {
-    return (
-      <>
-        {this.state.isMenuOpen && this.renderMenu()}
+	render() {
+		return (
+			<>
+				{/* {this.state.isMenuOpen && this.renderMenu()} */}
 
-        <div
-          className={`homepage--container ${
-            this.state.currentPage === 'HOME'
-              ? `nobody-knows-bg`
-              : `nobody-knows-bg`
-          }
+				<div
+					className={`homepage--container bg-dallas-wave
            pa2 flex flex-column items-center`}
-        >
-          <Nav toggleMenu={this.toggleMenu} />
-          <Switch>
-            <Route
-              exact
-              path='/'
-              render={() => <Home setCurrentPage={this.setCurrentPage} />}
-            />
-            {/* <Route
+				>
+					{/* <Nav toggleMenu={this.toggleMenu} /> */}
+					<Switch>
+						<Route
+							exact
+							path="/"
+							render={() => <Home setCurrentPage={this.setCurrentPage} />}
+						/>
+						{/* <Route
               path='/nobody-knows'
               render={() => (
                 <NobodyKnows setCurrentPage={this.setCurrentPage} />
@@ -61,16 +57,16 @@ class HomePage extends Component {
               path='/credits'
               render={() => <Credits setCurrentPage={this.setCurrentPage} />}
             /> */}
-            <Route
-              path='/watch'
-              render={() => <Watch setCurrentPage={this.setCurrentPage} />}
-            />
-            <Redirect to='/' />
-          </Switch>
-        </div>
-      </>
-    );
-  }
+						{/* <Route
+							path="/watch"
+							render={() => <Watch setCurrentPage={this.setCurrentPage} />}
+						/> */}
+						<Redirect to="/" />
+					</Switch>
+				</div>
+			</>
+		);
+	}
 }
 
 export default HomePage;
