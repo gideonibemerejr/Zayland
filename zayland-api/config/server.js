ee6284a68ae039ecf812bba571c5e7cb2dbcf7e3
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2021-03-28 21:26:50 -0500

    adds env, mongo db atlas, and s3 uploads

---
+++ zayland-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9b61936e3e03a19fd7481b8ab307becc'),
+    },
+  },
+});
---
+++ zayland-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f1a191e2-d0cb-42ae-90ac-10c753f4690f'
+};
